8599e0307989c0e1e25a814b2ad4701d9fa9c9b2
John Doe <johndoe@example.com>
2024-11-13 09:05:57 +0000

    primero

---
+++ script/env.exapmle.sh
@@ -0,0 +1,12 @@
+#Configuramos las varieblaes de entorno
+PHPMYADMIN_APP_PASSWORD=
+# Variables de configuración MySQL
+DB_USER=
+DB_PASSWORD=
+DB_NAME=
+# Creacion de usuario 
+STATS_USERNAME=
+STATS_PASSWORD=
+##
+LE_EMAIL=
+LE_DOMAIN=
